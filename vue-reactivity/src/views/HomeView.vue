b0cea083c607308bd6fe2dd5b18f4bf742ce67a6
tiffyy101 <bobatiffy101@gmail.com>
2023-03-16 10:16:09 -0400

    2

--- vue-reactivity/src/views/HomeView.vue
+++ vue-reactivity/src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
 <div class="home">
-  <h1>Hi</h1>
+  <h1 v-if="graduated">{{ student }}</h1>
+  <h1 v-else>Not yet graduated"></h1>
+  <ul>
+    <li v-for="animal in animals" :key="animal">{{ animal }}</li>
+  </ul>
 </div>
 </template>
 
 <script>
-
+export default {
+  name: "Home",
+  components: {},
+  data(){
+    return{
+      student:"WEEEE",
+      graduated: true,
+      animals: ["heeee", "neeeee", "beeeee"]
+    }
+  }
+};
 </script>
 <style>
 </style>
